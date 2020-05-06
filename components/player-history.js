5f0e048a835e0aa706c999a3f85eef82180460fa
mmcknett <mmcknett@godaddy.com>
2020-05-05 22:15:26 -0700

    Fix lint, update tests

--- components/player-history.js
+++ components/player-history.js
@@ -5,46 +5,51 @@ const PropTypes = require('prop-types');
 const { Box, Text, Color } = require('ink');
 const TextInput = require('ink-text-input').default;
 
-function PlayerHistory({name, isActive, done}) {
+function PlayerHistory({ name, isActive, done }) {
   const [inputText, setInputText] = useState('');
   const [commandList, setCommandList] = useState([]);
 
-  const handleChange = text => {
+  const handleChange = (text) => {
     setInputText(text);
   };
 
-  const handleSubmit = text => {
+  const handleSubmit = (text) => {
     if (text.toLowerCase() === 'done') {
       setInputText('');
       done();
-    }
-    else
-    {
+    } else {
       setInputText('');
       setCommandList([...commandList, text]);
     }
-  }
+  };
 
   return (
     <Box flexDirection="column" alignItems="flex-start" marginLeft={2} marginRight={2} width={15}>
       <Box marginBottom={1}>
         <Text>
-          <Color blue>{name}</Color>'s guesses
+          <Color blue>{name}</Color>
+          &apos;s guesses
         </Text>
       </Box>
+      { /* eslint-disable-next-line react/no-array-index-key */ }
       { commandList.map((command, idx) => <Text key={`${command}${idx}`}>{ command }</Text>) }
-      { isActive && <TextInput value={inputText} onChange={handleChange} onSubmit={handleSubmit} /> }
+      {
+        isActive
+        && <TextInput value={inputText} onChange={handleChange} onSubmit={handleSubmit} />
+      }
     </Box>
   );
 }
 
 PlayerHistory.propTypes = {
   name: PropTypes.string,
-  isActive: PropTypes.bool.isRequired
-}
+  isActive: PropTypes.bool.isRequired,
+  done: PropTypes.function,
+};
 
 PlayerHistory.defaultProps = {
   name: 'Stranger',
+  done: () => {},
 };
 
 module.exports = PlayerHistory;
